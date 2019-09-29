16f1648d571b7287101f2d2049d895478551bc7d
Jimothy <phazonjim@gmail.com>
2019-09-29 16:10:43 -0400

    Big Refactor
     - Added requirements.txt
     - Refactored globals
     - Added check to ensure post was truly removed

--- RemovalBot.py
+++ RemovalBot.py
@@ -8,34 +8,33 @@ from pprint import pprint
 from praw.models import MoreComments
 
 #===Globals===#
-#Reddit PRAW Object
-reddit = None
-#Removal Reasons Pulled from wiki
-removalReasons = None
-#Object to keep track of submissions
-submissions = {}
-#Object containing list of submissions already dealt with 
-postCache = None
-postCacheName = None
 #Config file
 config = None
 
-def init():
+def loadConfig():
     global config
-    global reddit
-    global postCacheName
-    global postCache
-    global removalReasons
+    #Load configs
+    try:
+        config = yaml.load(open(os.path.join(os.path.dirname(__file__),'config.yaml')).read(), Loader=yaml.FullLoader)
+    except:
+        print("'config.yaml' could not be located. Please ensure 'config.example' has been renamed")
+        exit()
 
-    config = yaml.load(open('config.yaml').read())
+def initReddit():
     client = config['client']
     reddit = praw.Reddit(**client)
-    removalReasons = getRemovalReasons()
-    #Load in post cache
+    return reddit
+
+def loadCache():
+    postCache = {}
     postCacheName = os.path.join(os.path.dirname(__file__), "cache.json")
-    with open(postCacheName, 'r') as fin:
-        cache = fin.read()
-    postCache = json.loads(cache)
+    try:
+        with open(postCacheName, 'r') as fin:
+            cache = fin.read()
+        postCache = json.loads(cache)
+    except Exception as e:
+        print (e)
+    return postCache
 
 def getRuleFromRegexMatch(flair):
     #Get regex patterns from the config file
@@ -50,7 +49,7 @@ def getRuleFromRegexMatch(flair):
         if (re.search(regexes[regex], flair)):
             return regex
 
-def getRemovalReasons():
+def getRemovalReasons(reddit):
     #Grab the removal reasons from the wiki
     wikiPage = reddit.subreddit(config['wiki_subreddit']).wiki[config['removal_reasons_wiki']].content_md
     return yaml.load(wikiPage)
@@ -61,11 +60,11 @@ def checkForDuplicateComments(submissionObj):
         if isinstance(top_level_comment, MoreComments):
             continue
         #If there is a top level comment from the bot username, return true
-        if top_level_comment.author == config['client']['username']:
+        if top_level_comment.author == 'PhazonJim':
             return True
     return False
 
-def postComment(submissionObject, submissionRule):
+def postComment(submissionObject, submissionRule, removalReasons):
     #Build up comment body from wiki
     commentBody = ''
     commentBody += removalReasons['header']
@@ -83,40 +82,47 @@ def postComment(submissionObject, submissionRule):
         #If anything bad happens, return back false
         return False
 
-def saveCache():
+def saveCache(postCache):
+    postCacheName = os.path.join(os.path.dirname(__file__), "cache.json")
     with open(postCacheName, 'w') as fout:
         for chunk in json.JSONEncoder().iterencode(postCache):
             fout.write(chunk)
 
 if __name__ == '__main__':
-    #Initiate a bunch of stuff
-    init()
+    #load configuration yaml
+    loadConfig()
+    #Load in post cache
+    postCache = loadCache()
     #Get moderator object from PRAW
+    reddit = initReddit()
     moderator = reddit.subreddit(config['subreddit']).mod
+    #Load Removal Reasons
+    removalReasons = getRemovalReasons(reddit)
+    #Local vars
+    submissions = {}
     #Only check for removelink actions, grab last X since we dont want to spend too much time grabbing post information
     for log in moderator.log(action='removelink',limit=config['mod_log_depth']):
         #Don't check for removals by AutoMod
         if log._mod not in config['ignore_mods']:
             #Submission ID from mod log
             submissionId = log.target_fullname.split('_')[1]
-            submissions[submissionId] = ''
-    #Figure out the rules that were flaired in each submission
-    for submission in submissions:
-        #If the submission has already been commented on, we don't need to worry about leaving a comment
-        if submission not in postCache:
-            submissionObject = reddit.submission(id=submission)
-            submissionFlair = submissionObject.link_flair_text
-            rule = getRuleFromRegexMatch(submissionFlair)
-            if rule:
-                #If a rule was found in the flair, add the rule and submission object to our submissions dict
-                submissions[submission] = {"rule": rule, "_self": submissionObject}
+            if submissionId not in postCache:
+                submissionObject = reddit.submission(id=submissionId)
+                if submissionObject.removed:
+                    submissionFlair = submissionObject.link_flair_text
+                    rule = getRuleFromRegexMatch(submissionFlair)
+                    if rule:
+                        #If a rule was found in the flair, add the rule and submission object to our submissions dict
+                        submissions[submissionId] = {"rule": rule, "_self": submissionObject}
+
     #Leave comments
     for submission in submissions:
         submissionObject = submissions[submission]['_self']
         submissionRule = submissions[submission]['rule']
         if not checkForDuplicateComments(submissionObject):
-            result = postComment(submissionObject, submissionRule)
+            result = postComment(submissionObject, submissionRule, removalReasons)
             if result:
                 postCache[submission] = result.id
     #Write out
-    saveCache()
+    if postCache:
+        saveCache(postCache)
